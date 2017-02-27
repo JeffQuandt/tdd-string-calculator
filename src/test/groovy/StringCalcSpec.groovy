8c5a82bc8b5257eb867ccf1bae5098e6551796b1
JeffQuandt <6EKz%wC7*6$TVW6#xm>
2017-02-26 19:50:05 -0600

    initial test

--- src/test/groovy/StringCalcSpec.groovy
+++ src/test/groovy/StringCalcSpec.groovy
@@ -2,5 +2,15 @@ import spock.lang.Specification
 
 class StringCalcSpec extends Specification{
 
+    StringCalc calc = new StringCalc()
+
+    def "should return 0 for empty string"() {
+
+        when:
+        def sum = calc.add("")
+
+        then:
+        sum == 0
+    }
 
 }
