1ea437fd0904effcc496baac5f3c5d9339f97df9
JeffQuandt <6EKz%wC7*6$TVW6#xm>
2017-02-26 20:21:08 -0600

    two number implementation

--- src/main/groovy/StringCalc.groovy
+++ src/main/groovy/StringCalc.groovy
@@ -3,7 +3,16 @@ class StringCalc {
     Integer add(String numbers) {
         def sum = 0
         if (numbers) {
-            sum += Integer.valueOf(numbers)
+            def index = numbers.indexOf(',')
+            String num1 = numbers
+            if (index != -1) {
+                num1 = numbers.substring(0, index)
+                if (index < numbers.length()) {
+                    def num2 = numbers.substring(index + 1, numbers.length())
+                    sum += Integer.valueOf(num2)
+                }
+            }
+            sum += Integer.valueOf(num1)
         }
         sum
     }
